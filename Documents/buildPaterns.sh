07a99470a90042cbf7a3f78c9ce02c0725547dbe
denproger <denproger@gmail.com>
2016-10-11 21:17:19 +0300

    Перенос документации в другую директорию

---
+++ Documents/buildPaterns.sh
@@ -0,0 +1,9 @@
+#java -jar plantuml.jar -tsvg afactory.txt
+#java -jar plantuml.jar -tpng afactory.txt
+#java -jar plantuml.jar -tsvg facade.txt
+#java -jar plantuml.jar -tpng facade.txt
+#java -jar plantuml.jar -tsvg adapter.txt
+#java -jar plantuml.jar -tpng adapter.txt
+java -jar plantuml.jar -tsvg state.txt
+java -jar plantuml.jar -tpng state.txt
+
